6a159a83db47f4450880f2b0d6fdb18ddedbf516
PedroEduardoSS <pedroeduardossousa@gmail.com>
2022-11-10 10:32:05 -0300

    Update teste

--- teste/src/teste/Teste.java
+++ teste/src/teste/Teste.java
@@ -1,7 +1,8 @@
 package teste;
 
 public class Teste {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        System.out.println(str);
     }
 }
